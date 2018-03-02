e6196c2b3472db406a0a2cfd82adf31b97f64b49
Gergerich <t9067836006@gmail.com>
2018-03-02 19:20:30 +0300

    Установка и настройка сфинкса по средствам вагранта

---
+++ vagrant/scripts/install-sphinxsearch.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+echo "install Sphinxsearch engine"
+
+# Check if Elasticsearch has been installed
+
+if [ -f /home/vagrant/.sphinxsearch ]
+then
+    echo "Sphinxsearch already installed."
+    exit 0
+fi
+
+touch /home/vagrant/.sphinxsearch
+
+# Install Sphinxsearch
+sudo apt-get update
+sudo apt-get -y install sphinxsearch
+
+#Configure Sphinxsearch
+sudo cp -f /tmp/sphinx.conf /etc/sphinxsearch/sphinx.conf
+sudo sed -i "s/START=no/START=yes/" /etc/default/sphinxsearch
+
+# Start Sphinxsearch on boot
+sudo update-rc.d sphinxsearch defaults 95 10
+
+# Start Elasticsearch
+sudo service sphinxsearch restart
+sudo -u sphinxsearch indexer --all
